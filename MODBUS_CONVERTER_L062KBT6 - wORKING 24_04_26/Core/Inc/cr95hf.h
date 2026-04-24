ba114578b09af0052b4d2d950cd9ab0d04e37a44
Anurag02530 <aimilanurag@gmail.com>
2026-04-24 17:43:36 +0530

    24_04_26

---
+++ MODBUS_CONVERTER_L062KBT6 - wORKING 24_04_26/Core/Inc/cr95hf.h
@@ -0,0 +1,70 @@
+#ifndef CR95HF_SIMPLE_H
+#define CR95HF_SIMPLE_H
+
+#include "main.h"
+
+#define UART1_RX_BUFFER_SIZE   600
+#define UART2_RX_BUFFER_SIZE   600
+
+//typedef struct
+//{
+//    
+
+//    // ?? Time fields (2 bytes each)
+//    uint16_t second;
+//    uint16_t minute;
+//    uint16_t hour;
+//    uint16_t date;
+//    uint16_t month;
+//    uint16_t year;
+//		uint16_t base_unit;
+//		double total_volume;
+//		double forward_volume;
+//    double reverse_volume;
+//    float flow;
+//		float temperature;
+//		uint32_t serial_no;
+//		uint16_t crc;
+//    
+
+//} MeterData_t;
+
+
+   
+		
+    
+
+    
+
+// ================= GLOBALS =================
+//extern UART_HandleTypeDef hlpuart3;  // CR95HF UART
+//extern uint8_t rxBuf[32];            // Last received data
+//extern uint8_t rxLen;                // Number of bytes expected
+//extern uint8_t rxDone;               // RX complete flag
+//extern uint8_t uid[10];              // UID storage
+//extern uint8_t uidLen;               // UID length
+
+// ================= FUNCTIONS =================
+void CR95HF_UART_Init(void);   // USART1 for CR95HF
+void DEBUG_UART_Init(void);		//USART2 for RS485/DEBUG
+static void CR95HF_Send(uint8_t *cmd, uint8_t tx, uint8_t rx);  // Send command and start RX
+void CR95HF_Process(void);     // Call repeatedly in main loop to handle state machine
+void Process_Frame(uint8_t *rxBuffer, uint16_t rxLen);
+void DBG_Print(const char *s);
+void DBG_PrintHex(uint8_t *data, uint16_t len);
+void Read_Meter(void);
+void CR95HF_Process1(void);
+void CR95HF_Read_Memory(void);
+void DBG_PrintASCII(uint8_t *buf, uint16_t len);
+void Decode_Data(void);
+void build_frame(uint8_t *tx);
+
+void handle_modbus_write(uint8_t *rx);
+void write_modbus_address(uint8_t addr);
+uint8_t read_modbus_address(void);
+uint16_t modbus_crc(uint8_t *buf, uint16_t len);
+
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
+
+#endif // CR95HF_SIMPLE_H
---
+++ MODBUS_CONVERTER_L062KBT6 - wORKING 24_04_26/Core/Src/stm32l0xx_it.c
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32l0xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l0xx_it.h"
+
+
+
+
+
+/******************************************************************************/
+/*           Cortex-M0+ Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+
+   while (1)
+  {
+  }
+
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+
+  HAL_IncTick();
+
+}
+
+/******************************************************************************/
+/* STM32L0xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32l0xx.s).                    */
+/******************************************************************************/
+
+
+
+
